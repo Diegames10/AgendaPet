0c627fd70fe1cec8621508087744fbc042c94858
Diegames10 <diegoandrioli_48@hotmail.com>
2026-07-21 22:36:30 -0300

    Remover pasta duplicada e arquivos temporarios

--- AgendaPetPgua/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .usuario import Usuario
-from .pet import Pet
-from app.models.agendamentoConsulta import AgendamentoConsulta
-from app.models.veterinario import Veterinario
-from app.models.historico import Historico
